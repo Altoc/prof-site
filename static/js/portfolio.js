0362db016516aac93074cd79781124a3c0e97283
altoc <iwhitese@kent.edu>
2019-03-21 13:47:56 -0400

    adding changes

--- static/js/portfolio.js
+++ static/js/portfolio.js
@@ -2,7 +2,7 @@ window.onload = function (){
   document.getElementById("button_Portfolio").onclick = function () {window.location.href='./portfolio.html'};
   document.getElementById("button_ContactUs").onclick = function () {window.location.href='./contact.html'};
   document.getElementById("button_Home").onclick = function () {window.location.href='./index.html'};
-  
+
   //document.getElementById("page_antiSocialMode").style.display = "none";
 }
 
@@ -12,18 +12,18 @@ function projectsToggle(x){
 		case 2: var element = document.getElementById("page_towerOfHanoi"); break;
 		case 3: var element = document.getElementById("page_nameGenerator"); break;
 		case 4: var element = document.getElementById("page_saveNewton"); break;
-		case 5: var element = document.getElementById("page_TTVM"); break;
+	//	case 5: var element = document.getElementById("page_TTVM"); break;
 		case 6: var element = document.getElementById("page_speakEasy"); break;
 		case 7: var element = document.getElementById("page_MIPS"); break;
 		case 8: var element = document.getElementById("page_cPlusPlus"); break;
 	}
-	
+
 	if(!element.style.display || element.style.display == "none"){
 		page_antiSocialMode.style.display = "none";
 		page_towerOfHanoi.style.display = "none";
 		page_nameGenerator.style.display = "none";
 		page_saveNewton.style.display = "none";
-		page_TTVM.style.display = "none";
+	//	page_TTVM.style.display = "none";
 		page_speakEasy.style.display = "none";
 		page_MIPS.style.display = "none";
 		page_cPlusPlus.style.display = "none";
--- static/js/tower_of_hanoi.js
+++ static/js/tower_of_hanoi.js
@@ -2,24 +2,24 @@ function tower_update(){	//update the tower instruction
 	var numberOfRings = document.getElementById("input_rings").value;
 	var ringNum = numberOfRings;
 	var moveNum = 0;
-	
+
 	var poleOne = [0];
 	var poleTwo = [0];
 	var poleThree = [0];
-	
+
 	var outputStack = [0];
 	var outputStackItr = 0;
-	
+
 	for(var i = 1; i <= ringNum; i++){
 		poleOne.push(i);
 	}
-	
-    var div = document.createElement('div');
+
+	var div = document.createElement('div');
 	var p = document.createElement('p');
-	
+
 	div.innerHTML = "Generating your steps...";
 	document.getElementById('playable_tower').appendChild(div);
-	
+
 	while((poleOne[poleOne.length-1] != 0) || (poleTwo[poleTwo.length-1] != 0)){
 		if(ringNum % 2 == 0){	//If number of rings is even
 			if(poleOne[poleOne.length-1] > poleTwo[poleTwo.length-1]){		//Make the legal move between poles 1 and 2:
@@ -35,7 +35,7 @@ function tower_update(){	//update the tower instruction
 				poleOne.push(poleTwo[poleTwo.length-1]);	//copy top ring of pole two to pole one,
 				poleTwo.pop();			//remove the original ring from ring two
 			}
-			
+
 			moveNum++;		//count number of moves
 
 			if(poleOne[poleOne.length-1] > poleThree[poleThree.length-1]){		//Make the legal move between poles 1 and 3:
@@ -53,7 +53,7 @@ function tower_update(){	//update the tower instruction
 			}
 
 			moveNum++;		//count number of moves
-	
+
 			if(poleTwo[poleTwo.length-1] > poleThree[poleThree.length-1]){		//Make the legal move between poles 2 and 3:	
 				outputStack[outputStackItr] = "Moving ring of size " + poleTwo[poleTwo.length-1] + " from pole 2 to pole 3";
 				outputStackItr++;
@@ -67,7 +67,7 @@ function tower_update(){	//update the tower instruction
 				poleTwo.push(poleThree[poleThree.length-1]);	//copy top ring of pole three to pole two,
 				poleThree.pop();		//remove the original ring from ring three
 			}
-	
+
 			moveNum++;		//count number of moves
 		}else if(ringNum % 2 != 0){			//else if number of rings is odd
 
@@ -84,11 +84,11 @@ function tower_update(){	//update the tower instruction
 				poleOne.push(poleThree[poleThree.length-1]);	//copy top ring of pole three to pole one,
 				poleThree.pop();		//remove the original ring from ring three
 			}
-			
+
 			moveNum++;		//count number of moves
-			
+
 			if((poleOne[poleOne.length-1] != 0) || (poleTwo[poleTwo.length-1])){
-				
+
 				if(poleOne[poleOne.length-1] > poleTwo[poleTwo.length-1]){		//Make the legal move between poles 1 and 2:
 					poleTwo.push(poleOne[poleOne.length-1]);	//copy top ring of pole one to pole two,
 					outputStack[outputStackItr] = "Moving ring of size " + poleOne[poleOne.length-1] + " from pole 1 to pole 2";
@@ -102,9 +102,9 @@ function tower_update(){	//update the tower instruction
 					poleOne.push(poleTwo[poleTwo.length-1]);	//copy top ring of pole two to pole one,
 					poleTwo.pop();			//remove the original ring from ring two
 				}
-	
+
 				moveNum++;		//count number of moves
-	
+
 				if(poleTwo[poleTwo.length-1] > poleThree[poleThree.length-1]){		//Make the legal move between poles 2 and 3:	
 					outputStack[outputStackItr] = "Moving ring of size " + poleTwo[poleTwo.length-1] + " from pole 2 to pole 3";
 					outputStackItr++;
@@ -118,20 +118,20 @@ function tower_update(){	//update the tower instruction
 					poleTwo.push(poleThree[poleThree.length-1]);	//copy top ring of pole three to pole two,
 					poleThree.pop();		//remove the original ring from ring three
 				}
-		
-				moveNum++;		//count number of moves	
+
+				moveNum++;		//count number of moves
 			}
 		}
 	}
-	
+
 	for(var i = 0; i < moveNum; i++){
 		p.innerHTML = p.innerHTML + outputStack[i] + "<br />";
 	}
-	
+
 	document.getElementById('playable_tower').appendChild(p);
-	
+
 	var numberOfMoves = document.createElement('p');
-	
+
 	numberOfMoves.innerHTML = "Number of steps required: " + moveNum;
 	document.getElementById('playable_tower').appendChild(numberOfMoves);
 }
